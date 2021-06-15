1483ce43e48855ee6f0968deb71749bcb5fd14a8
varundevpro <28918411+VarunDevPro@users.noreply.github.com>
2021-06-15 06:46:45 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: "Quotes",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
